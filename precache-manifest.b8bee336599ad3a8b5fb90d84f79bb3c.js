d7f40a6f23bd8a7817c85db6a287e24577ff0478
Amit Kumar Chaudhary <117110990+amitchaudhary08@users.noreply.github.com>
2023-07-11 10:52:22 +0000

    Updates

---
+++ precache-manifest.b8bee336599ad3a8b5fb90d84f79bb3c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4680892ee715cf38c4cb614af03dfe3",
+    "url": "/Keeper-App/index.html"
+  },
+  {
+    "revision": "a4d931c63f47251f68ff",
+    "url": "/Keeper-App/static/js/2.260104d4.chunk.js"
+  },
+  {
+    "revision": "419364406daf6059f846",
+    "url": "/Keeper-App/static/js/main.a057f76c.chunk.js"
+  },
+  {
+    "revision": "4997685bab9cff726bba",
+    "url": "/Keeper-App/static/js/runtime-main.31b25eb3.js"
+  }
+]);
