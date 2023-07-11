7d35a213bd3c28a3005302897da03389d2b28d41
Amit Kumar Chaudhary <117110990+amitchaudhary08@users.noreply.github.com>
2023-07-11 10:46:50 +0000

    first commit

---
+++ build/precache-manifest.cccd62778504c3b1932dd077820aa69e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea8f1b80da19281cd33e77e12ffbfdd8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4d931c63f47251f68ff",
+    "url": "/static/js/2.260104d4.chunk.js"
+  },
+  {
+    "revision": "419364406daf6059f846",
+    "url": "/static/js/main.a057f76c.chunk.js"
+  },
+  {
+    "revision": "10e117344dea3db99e26",
+    "url": "/static/js/runtime-main.b5766938.js"
+  }
+]);
